4271efe3328fa178c85aa513985d729440e680e0
Aakash Mahajan <mahajanaakash108@gmail.com>
2025-03-25 16:41:30 +0530

    feat: authMiddleware and few more constants added

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from 'jsonwebtoken';
+import { JWT_CONFIG } from "@/config/jwt";
+import { UNAUTHORIZED, FORBIDDEN} from '@/config/http'
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) return res.status(UNAUTHORIZED).json({message: 'Unauthorized'});
+
+    try {
+        const decoded = jwt.verify(token, JWT_CONFIG.accessTokenSecret);
+        (req as any).user = decoded;
+        next();
+    } catch {
+        return res.status(FORBIDDEN).json({ message: 'Forbidden'});
+    }
+};
