e1d130f834d06f795d935fca33c5e68874210ec1
Aakash Mahajan <mahajanaakash108@gmail.com>
2025-03-25 17:09:55 +0530

    feat: controllers completed

--- src/api/controllers/authController.ts
+++ src/api/controllers/authController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import * as authService from '@/services/authService';
+import { UNAUTHORIZED, OK } from '@/config/http'
+import { generateAccessToken } from '@/utils/generateTokens';
 
 export const register = async (req: Request, res: Response) => {
     const { name, email, password } = req.body;
@@ -19,4 +21,20 @@ export const login = async (req: Request, res: Response) => {
     });
 
     res.json({ accessToken });
-}
+};
+
+export const refresh = async (req: Request, res: Response) => {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) return res.status(UNAUTHORIZED).json({ message: 'Missing refresh token'});
+    const decoded = await authService.verifyRefreshToken(refreshToken);
+    const newAccessToken = generateAccessToken({ userId: decoded.userId });
+
+    res.json({ accessToken: newAccessToken});
+};
+
+export const logout = async (req: Request, res: Response) => {
+    const userId = (req as any).user.userId;
+    await authService.logoutUser(userId);
+    res.clearCookie('refreshToken');
+    res.status(OK).json({ message: 'Logged Out'});
+};
