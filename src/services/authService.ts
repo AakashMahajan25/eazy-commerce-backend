a0bedbf9ab4f12a29a1a8be793178f09cc118d61
Aakash Mahajan <mahajanaakash108@gmail.com>
2025-03-25 16:24:45 +0530

    feat: authService completed

---
+++ src/services/authService.ts
@@ -0,0 +1,38 @@
+import { generateAccessToken, generateRefreshToken } from "@/utils/generateTokens";
+import { redisClient } from "@/config/redis"; 
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken'
+import { JWT_CONFIG } from "@/config/jwt";
+import { prisma } from "@/config/database";
+
+export const registerUser = async (name: string, email: string, password: string) => {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    return prisma.user.create({
+        data: {name, email, password: hashedPassword},
+    });
+};
+
+export const loginUser = async (email: string, password: string) => {
+    const user = await prisma.user.findUnique({ where: { email }});
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+        throw new Error('Invalid email or password');
+    }
+    const payload = { userId: user.id, email: user.email };
+    const accessToken = generateAccessToken(payload);
+    const refreshToken = generateRefreshToken(payload);
+
+    await redisClient.setex(user.id.toString(), JWT_CONFIG.refreshTokenExpiry! ,refreshToken);
+
+    return { accessToken, refreshToken };
+}
+
+export const verifyRefreshToken = async (token: string) => {
+    const decoded = jwt.verify(token, JWT_CONFIG.refreshTokenSecret) as { userId: string };
+    const storedToken = await redisClient.get(decoded.userId);
+    if (storedToken !== token) throw new Error('Invalid refresh token');
+    return decoded;
+};
+
+export const logoutUser = async (userId: string) => {
+    await redisClient.del(userId);
+};
