135ce203854283492b6b9baa8456930e53d501b2
Aakash Mahajan <mahajanaakash108@gmail.com>
2025-03-30 20:52:04 +0530

    feat: product services expanded

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,10 +1,30 @@
 import { ProductModel } from "@/models/product.model";
 
+// getAllProducts, createProduct, updateProduct, deleteProduct, 
+
 export const getAllProducts = async () => {
     return await ProductModel.findMany();
 };
 
-export const createProduct = async (name: string, slug: string, description: string, price: number, stock: number, categoryId: number, category: string, images?: string[]) => {
+export const createProduct = async (name: string, slug: string, description: string, price: number, stock: number, categoryId: number, images?: string[]) => {
+    return ProductModel.create({
+        data: {
+            name,
+            slug,
+            description,
+            price,
+            stock,
+            categoryId,
+            images
+        }
+    })
+};
 
+export const deleteProduct = async (productId: number) => {
+    ProductModel.delete({
+        where: {
+            id: productId
+        }
+    })
 };
 
